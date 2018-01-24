db904469932dc89e0637e0b9d24ce873b170ef24
arthur <arthur.teisseire@epitech.eu>
2018-01-24 11:48:39 +0100

    my_str_isalphanul added to lib

--- lib/my/my.h
+++ lib/my/my.h
@@ -21,5 +21,6 @@ int my_put_nbr(int nb);
 int my_strcmp(char const *str1, char const *str2);
 char *my_strdup(char *str);
 int my_strncmp(char const *s1, char const *s2, int n);
+int my_str_isalphanum(char *str);
 
 #endif
