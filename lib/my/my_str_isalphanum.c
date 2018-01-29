cfbc60fc5c2bce19ec69a42c6b980f03faa542bd
arthur <arthur.teisseire@epitech.eu>
2018-01-29 15:28:58 +0100

    trailing space deleted

--- lib/my/my_str_isalphanum.c
+++ lib/my/my_str_isalphanum.c
@@ -10,7 +10,7 @@ int my_str_isalphanum(char *str)
 	int i = 0;
 
 	while (str[i] != '\0') {
-		if ((str[i] >= 'a' && str[i] <= 'z') 
+		if ((str[i] >= 'a' && str[i] <= 'z')
 		|| (str[i] >= 'A' && str[i] <= 'Z')
 		|| (str[i] >= '0' && str[i] <= '9')
 		|| (str[i] == '_'))
